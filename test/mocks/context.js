ee006ae14fa47aec72522b4e9644f67241dbc89d
Jared Evans <jarede@gmail.com>
2024-09-28 09:27:27 +0100

    create test mocks

---
+++ test/mocks/context.js
@@ -0,0 +1,16 @@
+const context = {
+  callbackWaitsForEmptyEventLoop: true,
+  functionName: "readOne",
+  functionVersion: "0.0.1",
+  invokedFunctionArn: "",
+  memoryLimitInMB: "",
+  awsRequestId: "",
+  logGroupName: "",
+  logStreamName: "",
+  getRemainingTimeInMillis: () => 109,
+  done: () => {},
+  fail: () => {},
+  succeed: () => {},
+};
+
+module.exports = context;
